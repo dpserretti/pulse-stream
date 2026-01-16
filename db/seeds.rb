cc393b11499e33e57d859945bcdddc0f6f321df2
dpserretti <danielserretti@gmail.com>
2026-01-15 22:48:38 -0300

    chore: add initial seeds for test tenant and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,24 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# This file contains the initial data required to run the application in development.
+# It creates a default tenant and an admin user for testing purposes.
+
+Rails.logger.info "\n== Seeding Database =="
+
+# 1. Create a Primary Tenant
+# We use 'streamer' as a subdomain to test http://streamer.lvh.me:3000
+tenant = Tenant.find_or_create_by!(subdomain: 'streamer') do |t|
+  t.name = 'Pro Streamer Studio'
+end
+Rails.logger.info "Created Tenant: #{tenant.name} (#{tenant.subdomain})"
+
+# 2. Create a User for this Tenant
+# The user must be associated with the tenant created above.
+User.find_or_create_by!(email: 'admin@pulsestream.com') do |user|
+  user.tenant = tenant
+  user.password = 'password123'
+  user.password_confirmation = 'password123'
+end
+Rails.logger.info 'Created User: admin@pulsestream.com (Password: password123)'
+
+Rails.logger.info "== Seeding Completed Successfully ==\n"
